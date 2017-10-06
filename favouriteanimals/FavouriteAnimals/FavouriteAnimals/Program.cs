77f8f8ede1cdafe0098db92b06b4a36c64bf79e5
csibvili <csibi.vili@gmail.com>
2017-10-06 10:36:45 +0200

    feat(read n write): favourite.txt has been readed and printed out in special format

---
+++ favouriteanimals/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TextOperations.PrintLines();
+            Console.ReadLine();
+        }    
+    }
+}
---
+++ favouriteanimals/FavouriteAnimals/FavouriteAnimals/TextOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    public class TextOperations
+    {
+        public static List<string> ReadText(string path)
+        {
+            var lines = new List<string>();
+            using (StreamReader file = new StreamReader(path))
+            {
+                string line = String.Empty;
+                while ((line = file.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            return lines;
+        }
+
+        public static void PrintLines()
+        {
+            Console.Write("C# FavouriteAnimals: ");
+            foreach (var line in ReadText("favourites.txt"))
+            {
+                Console.Write("[{0}] ",line);
+            }
+        }
+    }
+}
