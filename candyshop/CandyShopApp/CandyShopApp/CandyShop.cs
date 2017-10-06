6473d98999c85505dad7e787aaa9f11f30404604
csibvili <csibi.vili@gmail.com>
2017-10-06 11:15:24 +0200

    create architecture

---
+++ candyshop/CandyShopApp/CandyShopApp/CandyShop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShopApp
+{
+    public class CandyShop
+    {
+        public int Money, Sugar;
+        public List<Sweets> Sweets;
+
+        public CandyShop(int sugar)
+        {
+            Sugar = sugar;
+        }
+
+        public void CreateSweets(Sweets sweet)
+        {
+
+        }
+
+        public void Sell(Sweets sweet, int pieces)
+        {
+
+        }
+
+        public void PrintInfo()
+        {
+
+        }
+
+        public void BuySugar(int sugar)
+        {
+
+        }
+    }
+}
