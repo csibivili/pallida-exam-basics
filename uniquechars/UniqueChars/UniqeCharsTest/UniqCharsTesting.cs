ebbd65c9a3d971aca24ec54612c36a399b5ea2da
csibvili <csibi.vili@gmail.com>
2017-10-06 10:00:05 +0200

    first passed test added

---
+++ uniquechars/UniqueChars/UniqeCharsTest/UniqCharsTesting.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueChars;
+
+
+namespace UniqeCharsTest
+{
+    [TestFixture]
+    public class UniqCharsTesting
+    {
+        [Test]
+        public void TestUniqueCharactersWhenSingleLetter()
+        {
+            string input = "a";
+            var output = new List<char> { 'a' };
+
+            Assert.AreEqual(output, UniqueChars.UniqueChars.UniqueCharacters(input));
+        }
+    }
+}
--- uniquechars/UniqueChars/UniqueChars/Program.cs
+++ uniquechars/UniqueChars/UniqueChars/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UniqueChars
 {
-    class Program
+    public class UniqueChars
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // Create a method called `UniqueCharacters` that takes a string as parameter
+            // and returns a list with the unique letters of the given string
+            // Create basic unit tests for it with at least 3 different test cases
+            // Print the characters from that list in the following format:
+            // "n", "g", "r", "m"
+
+        }
+
+        public static List<char> UniqueCharacters(string input)
+        {
+            var UniqChars = new List<char>();
+            UniqChars.Add('a');
+            return UniqChars;
         }
     }
 }
